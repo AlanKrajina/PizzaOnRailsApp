d881426d9fcb444fb238fa1e59c758a5604df5df
AlanKrajina <alankrajina88@gmail.com>
2019-09-01 13:52:12 -0700

    application erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+    helper_method :admin_only, :current_user, :require_login
+
+    def admin_only
+        unless current_user.admin
+   #       flash[:notice] = "You must be an admin to perform that function!"
+          redirect_to user_path(current_user)
+        end
+      end
+    
+      def require_login
+    
+        unless current_user
+          redirect_to root_url
+        end
+    
+      end
+    
+      def current_user
+    
+        if session[:user_id].present?
+          user = User.find_by(:id => session[:user_id])
+        end
+    
+      end
+
+
 end
