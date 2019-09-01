794521251f666409e0a6bfe885e4bf3e860250a8
AlanKrajina <alankrajina88@gmail.com>
2019-09-01 13:39:19 -0700

    user session contr

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
+
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+
+  delete '/signout', to: 'sessions#destroy'
+
 end
