8d5dc26fa0d825ff81f51366cb11eac5a7b1b428
AlanKrajina <alankrajina88@gmail.com>
2019-09-04 13:40:50 -0700

    completed pizza validations

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -8,19 +8,21 @@ class Pizza < ApplicationRecord
     has_many :drinks
     has_many :toppings
 
-    validates :name,  presence: true
-    validates :delivery_address,  presence: true
-
     accepts_nested_attributes_for :rating
 
     validate :at_least_one_ingredient
+    validate :name_delivery_address
 
     def at_least_one_ingredient
       if [self.meats, self.cheeses, self.dips, self.toppings, self.drinks].reject(&:blank?).size == 0
-        errors[:base] << ("Please choose at least one meat, cheese, dip, topping or drink - we don't want to send you empty pizza crust :)")
+        errors[:base] << ("Please choose at least one meat, cheese, dip, topping or drink - we don't want to send you empty pizza crust. :)")
       end
     end  
 
-
+    def name_delivery_address
+      if [self.name, self.delivery_address].reject(&:blank?).size == 0
+        errors[:base] << ("Please submit a pizza name and delivery address.")
+      end
+    end 
 
 end
