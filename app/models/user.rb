f96723abe7bcce6f3412474b19f3f19eb1640ced
AlanKrajina <alankrajina88@gmail.com>
2019-09-04 13:57:56 -0700

    added signup validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,16 @@ class User < ApplicationRecord
     has_many :pizzas
     has_many :ratings, through: :pizzas
 
-    validates :name, :email, presence: true
     validates :name,  uniqueness: true
     validates :email, uniqueness: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+    validate :name_email_req
+
+    def name_email_req
+      if [self.name, self.email].reject(&:blank?).size == 0
+        errors[:base] << ("Please submit all a name and email to sign up.")
+      end
+    end 
+
 
 end
