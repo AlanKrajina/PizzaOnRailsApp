e0fd7c36a6ba20af4a9870eab60cca156cb2df90
AlanKrajina <alankrajina88@gmail.com>
2019-09-05 04:05:27 -0700

    added additional error valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
       end
     end 
 
+
     def self.from_omniauth(auth)
       where(email: auth.info.email).first_or_initialize do |user|
         user.name = auth.info.name
