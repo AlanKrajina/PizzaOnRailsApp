c96b61e1a02be11c92ebb92e620bdc721beb8d8a
AlanKrajina <alankrajina88@gmail.com>
2019-09-05 07:29:44 -0700

    css update

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,5 +2,4 @@ class Rating < ApplicationRecord
     has_many :pizzas
     has_many :users, through: :pizzas
     validates :stars, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, message: "shouldn't be blank and should be a number between 0 and 5" }
-
 end
