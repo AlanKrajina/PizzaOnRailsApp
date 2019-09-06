b5c87f8bcb803db42842d8949ab97281d0f50c09
AlanKrajina <alankrajina88@gmail.com>
2019-09-06 10:54:22 -0700

    user contr update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
       private
 
       def user_params
-        params.require(:user).permit(:name, :email, :password, :admin)
+        params.require(:user).permit(:name, :email, :password)
       end
   end
   
