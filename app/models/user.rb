6a2e3998664d2ca928ae60d5b7065e56ccaaf3f2
AlanKrajina <alankrajina88@gmail.com>
2019-09-06 09:40:13 -0700

    errors update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,16 @@ class User < ApplicationRecord
     has_many :ratings, through: :pizzas
 
     validates :name,  uniqueness: true
-    validates :email, uniqueness: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+    validates :name,  presence: true
+    validates :email, presence: true, uniqueness: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
-    validate :name_email_req
+  #  validate :name_email_req
 
-    def name_email_req
-      if [self.name, self.email].reject(&:blank?).size == 0
-        errors[:base] << ("Extra info* To create a User all details are needed.")
-      end
-    end 
+  #  def name_email_req
+  #    if [self.name, self.email].reject(&:blank?).size == 0
+  #      errors[:base] << ("Extra info* To create a User all details are needed.")
+  #    end
+  # end 
 
 
     def self.from_omniauth(auth)
