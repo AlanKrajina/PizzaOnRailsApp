a89acbea745d1cafed28e0fff7823e0298739908
AlanKrajina <alankrajina88@gmail.com>
2019-09-06 08:15:07 -0700

    css update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,15 @@
 class ApplicationController < ActionController::Base
-    helper_method :admin_only, :current_user, :require_login
+    helper_method :current_user, :require_login
     
       def require_login
-    
         unless current_user
           redirect_to root_url
         end
-    
       end
     
       def current_user
-    
         if session[:user_id].present?
           user = User.find_by(:id => session[:user_id])
         end
-    
       end
-
-
 end
